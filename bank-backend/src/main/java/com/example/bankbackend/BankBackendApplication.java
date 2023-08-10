1b1c669e37d69a2a036e8b9d5328b3e010342083
Aleksy Biela <aleks.302@wp.pl>
2023-08-11 00:07:02 +0200

    update

---
+++ bank-backend/src/main/java/com/example/bankbackend/BankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBackendApplication.class, args);
+	}
+
+}
