a0127af27fe842b1d1a30e68e8b64cea23146f94
pingu <aleks.302@wp.pl>
2023-08-30 00:20:37 +0200

    update

--- bank-backend/domain/src/main/java/com/example/bankbackend/transfer/TransferSnapshot.java
+++ bank-backend/domain/src/main/java/com/example/bankbackend/transfer/TransferSnapshot.java
@@ -1,4 +1,55 @@
 package com.example.bankbackend.transfer;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+
 public class TransferSnapshot {
+    private int id;
+    private String title;
+    private BigDecimal funds;
+    private int receiverId;
+    private Date transferDate;
+    private Time transferTime;
+
+    public TransferSnapshot() {
+    }
+
+    TransferSnapshot(final int id,
+                     final String title,
+                     final BigDecimal funds,
+                     final int receiverId,
+                     final Date transferDate,
+                     final Time transferTime) {
+        this.id = id;
+        this.title = title;
+        this.funds = funds;
+        this.receiverId = receiverId;
+        this.transferDate = transferDate;
+        this.transferTime = transferTime;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    BigDecimal getFunds() {
+        return funds;
+    }
+
+    int getReceiverId() {
+        return receiverId;
+    }
+
+    Date getTransferDate() {
+        return transferDate;
+    }
+
+    Time getTransferTime() {
+        return transferTime;
+    }
 }
