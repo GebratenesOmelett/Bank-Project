ea37f78d6097fba80b9ffa232410120e28e203c7
pingu <aleks.302@wp.pl>
2023-08-29 13:48:39 +0200

    update

--- bank-backend/adapters/src/main/java/com/example/bankbackend/config/SecurityFilterConfig.java
+++ bank-backend/adapters/src/main/java/com/example/bankbackend/config/SecurityFilterConfig.java
@@ -14,6 +14,6 @@ class SecurityFilterConfig {
                         .anyRequest().authenticated())
                 .csrf().disable();
         return httpSecurity.build();
-        //hello linux
+        //hello linux how are you
     }
 }
