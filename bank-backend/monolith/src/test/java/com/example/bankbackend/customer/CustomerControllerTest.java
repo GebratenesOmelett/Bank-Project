73fd7359e0d21697c92bb17305ada4503a00e18d
Aleksy Biela <aleks.302@wp.pl>
2023-09-16 21:52:23 +0200

    tests

--- bank-backend/monolith/src/test/java/com/example/bankbackend/customer/CustomerControllerTest.java
+++ bank-backend/monolith/src/test/java/com/example/bankbackend/customer/CustomerControllerTest.java
@@ -1,56 +1,77 @@
 package com.example.bankbackend.customer;
 
 import com.example.bankbackend.customer.dto.CustomerCreateDto;
-import com.example.bankbackend.customer.dto.CustomerDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application-test.properties")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CustomerControllerTest {
 
-    @Autowired
+
     private MockMvc mockMvc;
-    @Autowired
     private CustomerFacade customerFacade;
-    @Autowired
     private ObjectMapper objectMapper;
-    @Autowired
     private CustomerRoleRepository roleRepository;
 
 
-
+    @Autowired
+    public CustomerControllerTest(MockMvc mockMvc, CustomerFacade customerFacade, ObjectMapper objectMapper, CustomerRoleRepository roleRepository) {
+        this.mockMvc = mockMvc;
+        this.customerFacade = customerFacade;
+        this.objectMapper = objectMapper;
+        this.roleRepository = roleRepository;
+    }
 
     @BeforeEach
-    public void setupRoles(){
+    public void setupRoles() {
         if(roleRepository.findByRole("ROLE_USER").isEmpty()){
             roleRepository.save(new CustomerRole("ROLE_USER"));
             roleRepository.save(new CustomerRole("ROLE_ADMIN"));
         }
     }
-//    @Test
-//    @DisplayName("getShouldReturnCustomer")
-//    void get() {
-//        System.out.println("gaga");
-//    }
+
+    @Test
+    @DisplayName("getShouldReturnCustomer")
+    void get() throws Exception {
+        CustomerCreateDto customerCreateDto = new CustomerCreateDto(
+                "Pieter",
+                "Bark",
+                "testPassword",
+                "testPassword",
+                "Pieter@gmail.com"
+        );
+        customerFacade.create(customerCreateDto);
+
+        Assertions.assertNotNull(customerFacade.get(1));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/customers/{id}", 1))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.firstName", is("Pieter")))
+                .andExpect(jsonPath("$.lastName", is("Bark")))
+                .andExpect(jsonPath("$.email", is("Pieter@gmail.com")))
+                .andExpect(status().isOk()).andReturn();
+    }
 
     @Test
     void create() throws Exception {
@@ -87,3 +108,4 @@ class CustomerControllerTest {
 
     }
 }
+//ToDo napisz testy dla exceptionów OwO
