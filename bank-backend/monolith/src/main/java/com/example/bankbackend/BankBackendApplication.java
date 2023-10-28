4c2f4e7e5f3d9184a2a57dda55b7e2e217cfdecb
Aleksy Biela <aleks.302@wp.pl>
2023-10-28 23:27:24 +0200

    railway changes

--- bank-backend/monolith/src/main/java/com/example/bankbackend/BankBackendApplication.java
+++ bank-backend/monolith/src/main/java/com/example/bankbackend/BankBackendApplication.java
@@ -2,9 +2,15 @@ package com.example.bankbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
+@ComponentScan(basePackages = {"com.example"})
+@EntityScan(basePackages = {"com.example"})
+@EnableJpaRepositories(basePackages = {"com.example"})
 public class BankBackendApplication {
 
 	public static void main(String[] args) {
