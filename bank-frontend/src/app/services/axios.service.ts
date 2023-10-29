8017ef0d0bb69b584d28635c4ac0dd13379313a1
Aleksy Biela <aleks.302@wp.pl>
2023-10-29 20:22:33 +0100

    Frontend validation

--- bank-frontend/src/app/services/axios.service.ts
+++ bank-frontend/src/app/services/axios.service.ts
@@ -7,7 +7,7 @@ import axios from "axios";
 export class AxiosService {
 
   constructor() {
-    axios.defaults.baseURL = "http://localhost:8080"
+    axios.defaults.baseURL = "https://bank-project-production.up.railway.app"
     axios.defaults.headers.post["Content-Type"] = "application/json"
   }
 
