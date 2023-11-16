15af44da02438645dc7c76b3edce0d76a5a153db
Aleksy Biela <aleks.302@wp.pl>
2023-11-16 11:28:50 +0100

    railway changes

--- bank-backend/adapters/src/main/java/com/example/bankbackend/customer/SqlCustomerRepository.java
+++ bank-backend/adapters/src/main/java/com/example/bankbackend/customer/SqlCustomerRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-@Repository
+
 interface SqlCustomerRepository extends JpaRepository<CustomerSnapshot, Integer> {
     Optional<CustomerSnapshot> findCustomerById(int id);
 
